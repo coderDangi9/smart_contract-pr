90bd556e6bd34a7c1c3c062c993e22876bb31bf5
coderDangi9 <sandipdangi682@gmail.com>
2024-02-04 03:52:16 -0800

    pr1

--- tests/30_day project/day1/pr1.sol
+++ tests/30_day project/day1/pr1.sol
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier:MIT
-pragma solidity ^0.8.24;
-contract test{
-    uint public value;
-    function getdata(uint _val)public  returns(uint){
-      return (  value = _val);
+pragma solidity^0.8.24;
+contract Mapping{
+    // mapping(key_type => value_type)
+    mapping(address => uint) myMap;
+
+    function get(address _addr)public view returns(uint){
+        //return  on default value
+        return myMap[_addr];
     }
+    function set(address _addr,uint _x)public {
+        //return on a value of _x
+        myMap[_addr]=_x;
+    }
+    function remove(address _addr)public {
+        delete myMap[_addr];
+    }
+
 }
